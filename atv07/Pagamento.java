1277b17dd7f89c2b8ac9dbc3fb738fe25da8bd28
anacarolinaestevaodebarros7-af <anacarolinaestevaodebarros7@gmail.com>
2026-04-16 21:57:33 +0000

    commiti da  classe Atv07, pagamento, PagamentoCartao, PagamentoDinheiro

---
+++ atv07/Pagamento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atv07;
+
+/**
+ *
+ * @author curso
+ */
+public interface Pagamento {
+    double calcularTotal();
+    void imprimirRecibo();
+}
+ 
